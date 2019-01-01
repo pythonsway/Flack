178d3758b79479ff5a2498e442365e1ab680250b
Pythonsway <adam@pythonsway.it>
2019-01-01 21:29:21 +0100

    again

--- flack/__init__.py
+++ flack/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile('config.py', silent=True)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
